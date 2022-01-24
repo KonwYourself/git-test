79f1e46933f917a15e2cffca2a8f91e0eed338ab
xfc <xfc@qq.com>
2022-01-24 09:18:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author xfcforever
+ * @create 2022-01-24-9:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
